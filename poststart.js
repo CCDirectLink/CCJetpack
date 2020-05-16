1ca841443d3ad968f4649bf16961752b2c65960f
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2020-05-17 02:16:22 +0300

    make the key rebindable

---
+++ poststart.js
@@ -0,0 +1,1 @@
+ig.lang.labels.sc.gui.options.controls.keys.jump = 'Jump';
---
+++ prestart.js
@@ -0,0 +1,19 @@
+sc.OPTIONS_DEFINITION['keys-jump'] = {
+  type: 'CONTROLS',
+  init: { key1: ig.KEY.CTRL },
+  cat: sc.OPTION_CATEGORY.CONTROLS,
+};
+
+ig.ENTITY.Player.inject({
+  update(...args) {
+    if (ig.input.state('jump')) {
+      ig.game.playerEntity.doJump(
+        150, // zVel (vertical velocity)
+        16, // callbackHeight
+        250, // maxVel (max horizontal velocity in mid-air)
+      );
+    }
+
+    return this.parent(...args);
+  },
+});
