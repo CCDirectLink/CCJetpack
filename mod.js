65fbdc835ef5d433de614c5fe5ee4913d28fd294
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2020-05-16 15:12:05 +0300

    fix the CTRL key bug (closes #3)

--- mod.js
+++ mod.js
@@ -1,7 +1,15 @@
-document.body.addEventListener('modsLoaded', function () {
-	ig.input.bind(17, "jump");
-	simplify.registerUpdate(function(){
-		if(ig.input.state("jump"))
-			simplify.jumpHigh();
-	});
+ig.input.bind(ig.KEY.CTRL, 'jump');
+
+ig.ENTITY.Player.inject({
+  update(...args) {
+    if (ig.input.state('jump')) {
+      ig.game.playerEntity.doJump(
+        150, // zVel (vertical velocity)
+        16, // callbackHeight
+        250, // maxVel (max horizontal velocity in mid-air)
+      );
+    }
+
+    return this.parent(...args);
+  },
 });
